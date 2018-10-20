4efd8ded61cb0223727083bd94a5cdbcbadc47c3
Thesmader <dsmarak17@gmail.com>
2018-10-20 17:05:49 +0530

    The first commit.
    Signed-off-by: Thesmader <dsmarak17@gmail.com>

---
+++ app/src/main/java/com/thesmader/intelligenttrader/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.thesmader.intelligenttrader
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.toChatList))
+    }
+}
