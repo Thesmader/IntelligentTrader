62779eccaf6015b218c66a11b31223aca6746320
Thesmader <dsmarak17@gmail.com>
2018-10-26 08:57:01 +0530

    Firebase Email and Password authentication implemented.
    Signed-off-by: Thesmader <dsmarak17@gmail.com>

--- app/src/main/java/com/thesmader/intelligenttrader/ChatList.kt
+++ app/src/main/java/com/thesmader/intelligenttrader/ChatList.kt
@@ -3,27 +3,38 @@ package com.thesmader.intelligenttrader
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_chat_list.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class ChatList : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        //verifyUserLogin()
         return inflater.inflate(R.layout.fragment_chat_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
+    private fun verifyUserLogin() {
+        val uid = FirebaseAuth.getInstance().uid
+        Log.d("UID",uid)
+        if (uid == null){
+            findNavController().navigate(R.id.user_logged_out)
+        }
+
+        FirebaseAuth.getInstance().addAuthStateListener {
+            if (it.currentUser == null)
+                hello.findNavController().navigate(R.id.user_logged_out)
+        }
+    }
 }
--- app/src/main/java/com/thesmader/intelligenttrader/LoginFragment.kt
+++ app/src/main/java/com/thesmader/intelligenttrader/LoginFragment.kt
@@ -3,12 +3,26 @@ package com.thesmader.intelligenttrader
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_login.*
 
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
 class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +33,24 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loginBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.toChatList))
+        login_button.setOnClickListener{
+            loginUser(it)
+        }
+    }
+
+    private fun loginUser(loginBtn: View) {
+        val email = email_input_login.text.toString()
+        val pswrd = password_input_login.text.toString()
+
+        if (!email.isEmpty()&& !pswrd.isEmpty()){
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, pswrd).addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                else{
+                    Log.d("AuthLogin","User logged in successfully")
+                }
+            }
+            loginBtn.findNavController().navigate(R.id.action_loginFragment_to_chatList)
+        }
+        else Toast.makeText(loginBtn.context,"Fill in the fields", Toast.LENGTH_SHORT).show()
     }
 }
